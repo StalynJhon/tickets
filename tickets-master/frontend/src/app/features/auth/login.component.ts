12f99f95c82f78445ba249831b4d163cac90ab78
Brianna <bso.espinosa@yavirac.edu.ec>
2026-01-04 10:31:32 -0500

    Subiendo backend y frontend completos

---
+++ tickets-master/frontend/src/app/features/auth/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, RouterLink], // importante para que routerLink funcione
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+
+  constructor(private router: Router) {}
+
+  login() {
+    this.router.navigate(['/dashboard']);
+  }
+}
