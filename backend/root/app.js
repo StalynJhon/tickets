c2b1cb08e8f0cc0ff1c8cb1eb62ba346ebaa5036
StalynJhon <jsc.chisaguano@yavirac.edu.ec>
2026-01-06 17:58:33 -0500

    promociones funcionando

--- backend/root/app.js
+++ backend/root/app.js
@@ -266,6 +266,7 @@ app.use('/classification', require('../src/infrastructure/http/router/classifica
 app.use('/city', require('../src/infrastructure/http/router/city.router'));
 app.use('/country', require('../src/infrastructure/http/router/country.router'));
 app.use('/staff', require('../src/infrastructure/http/router/staff.router'));
+app.use('/promotions', require('../src/infrastructure/http/router/promotions.router'));
 
 app.use(require('../src/infrastructure/http/router/index'))
 
